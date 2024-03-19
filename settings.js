d3b60ae11b5398e4b52b4a876ffeae5530213243
Rubens Moreno Alves De Oliveira <RMorenoAlvesDeOliveira@geotechnics.co.nz>
2024-03-20 11:49:10 +1300

    test

---
+++ settings.js
@@ -0,0 +1,42 @@
+module.exports = {
+    uiPort: process.env.PORT || 1880,
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000,
+    debugMaxLength: 1000,
+    flowFile: 'flows.json',
+    adminAuth: {
+       type: "credentials",
+       users: [{
+          username: "cirro",
+          password: "cirro",
+          permissions: "*"
+       },
+       {
+          username: "admin",
+          password: "admin",
+          permissions: "*"
+       }]
+    },
+    functionGlobalContext: {
+    },
+    exportGlobalContextKeys: false,
+    logging: {
+       console: {
+          level: "info",
+          metrics: false,
+          audit: false
+       }
+    },
+    // Customising the editor
+    editorTheme: {
+       page: {
+          title: "Node-RED Flow Editor"
+       },
+       header: {
+          title: "Node-RED Flow Editor"
+       },
+       projects: {
+          enabled: true
+       }
+    }
+    }
